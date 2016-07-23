65d94c2d691b9366350174d572b73fb8fbfa893f
Christopher Antila <christopher@antila.ca>
2016-07-22 20:48:17 -0400

    Add main() function to init.py
    I'm about to add other things, might as well make the script proper.

--- init.py
+++ init.py
@@ -9,58 +9,60 @@ import sys
 VENV_PATH = 'ansible-venv'
 VENV_PROMPT = 'ncoda-ansible '
 
-# See if the virtualenv already exists, in which case they shouldn't us this script!
-if not os.path.exists(VENV_PATH):
-    # find the Python executable we'll use
-    if sys.version.startswith('2.7') and 'PyPy' not in sys.version:
-        python_exec = sys.executable
-    else:
-        print('Please start this script with CPython 2.7')
-        raise SystemExit(1)
-        # TODO: take the interpreter from the commandline
-        # TODO: try to look for an interpreter ourselves
 
-    # find the virtualenv executable we'll use
+def main():
+    # See if the virtualenv already exists, in which case they shouldn't us this script!
+    if not os.path.exists(VENV_PATH):
+        # find the Python executable we'll use
+        if sys.version.startswith('2.7') and 'PyPy' not in sys.version:
+            python_exec = sys.executable
+        else:
+            print('Please start this script with CPython 2.7')
+            raise SystemExit(1)
+            # TODO: take the interpreter from the commandline
+            # TODO: try to look for an interpreter ourselves
+
+        # find the virtualenv executable we'll use
+        try:
+            virtualenv_exec = subprocess.check_output(['which', 'virtualenv']).strip()
+        except subprocess.CalledProcessError:
+            print('\nPlease install "virtualenv" before running init.py\n')
+            raise SystemExit(1)
+
+        # make the virtualenv
+        try:
+            print('Creating a virtualenv for Ansible')
+            output = subprocess.check_output([
+                virtualenv_exec,
+                '-p',
+                python_exec,
+                '--prompt={0}'.format(VENV_PROMPT),
+                VENV_PATH
+            ])
+        except subprocess.CalledProcessError:
+            raise SystemExit(1)
+
+    activate = 'source {0}/bin/activate'.format(VENV_PATH)
+
     try:
-        virtualenv_exec = subprocess.check_output(['which', 'virtualenv']).strip()
+        subprocess.check_call('{0}; pip install -U setuptools pip'.format(activate), shell=True)
     except subprocess.CalledProcessError:
-        print('\nPlease install "virtualenv" before running init.py\n')
+        print('Failed while upgrading setuptools and pip')
         raise SystemExit(1)
 
-    # make the virtualenv
     try:
-        print('Creating a virtualenv for Ansible')
-        output = subprocess.check_output([
-            virtualenv_exec,
-            '-p',
-            python_exec,
-            '--prompt={0}'.format(VENV_PROMPT),
-            VENV_PATH
-        ])
+        subprocess.check_call('{0}; pip install -U "ansible<2.2"'.format(activate), shell=True)
     except subprocess.CalledProcessError:
+        print('Failed while installing Ansible')
         raise SystemExit(1)
 
-activate = 'source {0}/bin/activate'.format(VENV_PATH)
+    try:
+        subprocess.check_call('{0}; ansible-playbook -i .inventory .initialize.yml -t install_ncoda'.format(activate), shell=True)
+    except subprocess.CalledProcessError:
+        print('Failed while running "ansible-playbook"')
+        raise SystemExit(1)
 
-try:
-    subprocess.check_call('{0}; pip install -U setuptools pip'.format(activate), shell=True)
-except subprocess.CalledProcessError:
-    print('Failed while upgrading setuptools and pip')
-    raise SystemExit(1)
-
-try:
-    subprocess.check_call('{0}; pip install -U "ansible<2.2"'.format(activate), shell=True)
-except subprocess.CalledProcessError:
-    print('Failed while installing Ansible')
-    raise SystemExit(1)
-
-try:
-    subprocess.check_call('{0}; ansible-playbook -i .inventory .initialize.yml -t install_ncoda'.format(activate), shell=True)
-except subprocess.CalledProcessError:
-    print('Failed while running "ansible-playbook"')
-    raise SystemExit(1)
-
-print('''
+    print('''
 ********************************************************************************
 
 Finished initial setup (part 1)!
@@ -75,3 +77,7 @@ Before you continue, make sure you can pull the following repositories from Phab
 
 Then run this command: './nc init'
 ''')
+
+
+if __name__ == '__main__':
+    main()
